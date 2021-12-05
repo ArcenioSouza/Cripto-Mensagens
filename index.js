90d0dc08b26b55c5b260ebb2d158f9d5e5da382f
Arcenio Souza <arcenio_neto@icloud.com>
2021-12-05 07:41:49 -0300

    Adiciona mudança de descrição do button resultMessage

--- index.js
+++ index.js
@@ -1,62 +1,135 @@
-let modal = document.getElementById('modal')
+let modal = document.getElementById("modal");
 
-let btnCesar = document.getElementById('cesar')
+let btnCesar = document.getElementById("cesar");
 
-let btnModal = document.getElementById('btnModal')
+let btnModal = document.getElementById("btnModal");
 
-let btnBase64 = document.getElementById('base64')
+let btnBase64 = document.getElementById("base64");
 
-let btnCripto = document.getElementById('cripto')
+let btnCripto = document.getElementById("cripto");
 
-let btnDescripto = document.getElementById('descripto')
+let btnDescripto = document.getElementById("descripto");
 
-let incremento = document.getElementById('incremento')
+let resultMessage = document.getElementById("resultMessage");
 
-var valueIncrement = 0
+let incremento = document.getElementById("incremento");
 
-var alphabet = "abcdefghijklmnopqrstuvwxyzàèìòùáéíóúâêîôãõabcdefghijklmnopqrstuvwxyzàèìòùáéíóúâêîôãõ"
+let btnResult = document.getElementById("result");
 
+let valueIncrement = 0;
 
-btnCesar.addEventListener('click', () => {
-   modal.style.display = 'flex'
-   btnCesar.setAttribute("ativo", "ativo")
-   btnBase64.removeAttribute("ativo")
-})
+let alphabet =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzàèìòùáéíóúâêîôãõABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzàèìòùáéíóúâêîôãõ";
 
-btnBase64.addEventListener('click', () => {
-   btnBase64.setAttribute("ativo", "ativo")
-   btnCesar.removeAttribute("ativo")
-})
+btnCesar.addEventListener("click", () => {
+  modal.style.display = "flex";
+  btnCesar.setAttribute("ativo", "ativo");
+  btnBase64.removeAttribute("ativo");
+  if(btnCripto.hasAttribute('ativo') == true){
+    btnResult.innerText = "Criptografar com César"
+  }else{
+    btnResult.innerText = "Descriptografar com César"
+  }
+});
 
-btnCripto.addEventListener('click', () => {
-   btnCripto.setAttribute("ativo", "ativo")
-   btnDescripto.removeAttribute("ativo")
-})
+btnBase64.addEventListener("click", () => {
+  btnBase64.setAttribute("ativo", "ativo");
+  btnCesar.removeAttribute("ativo");
+  if(btnCripto.hasAttribute('ativo') == true){
+    btnResult.innerText = "Criptografar com Base64"
+  }else{
+    btnResult.innerText = "Descriptografar com Base64"
+  }
+});
 
-btnDescripto.addEventListener('click', () => {
-   btnDescripto.setAttribute("ativo", "ativo")
-   btnCripto.removeAttribute("ativo")
-})
+btnCripto.addEventListener("click", () => {
+  btnCripto.setAttribute("ativo", "ativo");
+  btnDescripto.removeAttribute("ativo");
+  if(btnCesar.hasAttribute('ativo') == true){
+    btnResult.innerText = "Criptografar com César"
+  }else{
+    btnResult.innerText = "Criptografar com Base64"
+  }
+});
 
-btnModal.addEventListener('click', () => {
-   modal.style.display = 'none'
-   valorIncremento = parseInt(incremento.value)
-})
+btnDescripto.addEventListener("click", () => {
+  btnDescripto.setAttribute("ativo", "ativo");
+  btnCripto.removeAttribute("ativo");
+  if(btnCesar.hasAttribute('ativo') == true){
+    btnResult.innerText = "Descriptografar com César"
+  }else{
+    btnResult.innerText = "Descriptografar com Base64"
+  }
+});
+
+btnModal.addEventListener("click", () => {
+  modal.style.display = "none";
+  valueIncrement = parseInt(incremento.value);
+});
+
+function codificarBase() {
+  var textMessage = document.getElementById("textMessage").value;
+  var binario = btoa(textMessage);
+  return binario;
+}
+
+function decodificarBase() {
+  var textMessage = document.getElementById("textMessage").value;
+  var binario = atob(textMessage);
+  return binario;
+}
 
 function codificarCesar() {
-   var textMessage = document.querySelector("#textMessage").value;
-   var textLowerCase = textMessage.toLowerCase();
-   var textCripto = "";
- 
-   for (var i = 0; i < textLowerCase.length; i++) {
-     for (var j = 0; j < alphabet.length; j++)
-       if (textLowerCase[i] == alphabet[j]) {
-         textCripto += alphabet[j + valueIncrement];
-         break;
-       } else if (textLowerCase[i] == " ") {
-         textCripto += " ";
-         break;
-       }
-   }
-   return textCripto;
- }
+  var textMessage = document.querySelector("#textMessage").value;
+  var textLowerCase = textMessage.toLowerCase();
+  var textCripto = "";
+
+  for (var i = 0; i < textLowerCase.length; i++) {
+    for (var j = 0; j < alphabet.length; j++)
+      if (textLowerCase[i] == alphabet[j]) {
+        textCripto += alphabet[j + valueIncrement];
+        break;
+      } else if (textLowerCase[i] == " ") {
+        textCripto += " ";
+        break;
+      }
+  }
+  return textCripto;
+}
+
+function decodificarCesar() {
+  var textMessage = document.querySelector("#textMessage").value;
+  var textLowerCase = textMessage.toLowerCase();
+  var textCripto = "";
+
+  for (var i = 0; i < textLowerCase.length; i++) {
+    for (var j = alphabet.length; j > 0; j--) {
+      if (textLowerCase[i] == alphabet[j]) {
+        textCripto += alphabet[j - valueIncrement];
+        break;
+      } else if (textLowerCase[i] == " ") {
+        textCripto += " ";
+        break;
+      }
+    }
+  }
+  return textCripto;
+}
+
+btnResult.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (btnCesar.hasAttribute("ativo") == true) {
+    if (btnCripto.hasAttribute("ativo") == true) {
+      var resposta = codificarCesar();
+    } else {
+      var resposta = decodificarCesar();
+    }
+  } else {
+    if (btnCripto.hasAttribute("ativo") == true) {
+      var resposta = codificarBase();
+    } else {
+      var resposta = decodificarBase();
+    }
+  }
+  resultMessage.value = `${resposta}`;
+});
